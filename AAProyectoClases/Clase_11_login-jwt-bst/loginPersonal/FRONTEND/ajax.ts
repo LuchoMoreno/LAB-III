0a47595dfc055f1304b4adf9090cc248547486a5
luchomoreno <lucho.moreno@live.com>
2019-11-15 11:27:02 -0300

    Actualizacion

---
+++ AAProyectoClases/Clase_11_login-jwt-bst/loginPersonal/FRONTEND/ajax.ts
@@ -0,0 +1,97 @@
+class Main{
+
+
+    public static Login() // EN CASO QUE HAYA ERROR, LE METO UNA e COMO PARAMETRO.
+    {
+        let xmlh : XMLHttpRequest = new XMLHttpRequest();
+
+        let correo = (<HTMLInputElement>document.getElementById("correo")).value;
+    
+        let clave = (<HTMLInputElement>document.getElementById("clave")).value;
+
+        // e.preventDefault(); // en caso que exista un error.
+
+        xmlh.onreadystatechange = () => 
+        {
+            if (xmlh.readyState == 4 && xmlh.status == 200) 
+            {
+
+                let recibido = JSON.parse(xmlh.responseText); // VOY A "DECODEAR" lo que recibi de la respuesta.
+    
+                if(recibido.exito == true)
+                {
+                    alert(recibido.jwt);
+                    localStorage.setItem("MiClave", recibido.jwt);
+
+                    //alert("Esta entrando a la pagina");
+                    //window.location.href = "principal.php";
+                }
+                else
+                {
+                    alert("No se encuentra el usuario");
+                }
+
+            }
+
+            if (xmlh.readyState == 4 && xmlh.status == 403) 
+            {
+                console.log(xmlh.responseText);
+            }
+
+        };
+        
+        xmlh.open("POST","http://localhost/loginPersonal/BACKEND/test_usuario.php/validarQueExista/",true);
+        //xmlh.open("POST","localhost/login/test_usuario.php/validarQueExista",true);
+    
+        xmlh.setRequestHeader("content-type","application/x-www-form-urlencoded");
+    
+        xmlh.send('usuario={"correo":"' + correo +'", "clave": "'+clave+'"}');
+        
+    }
+
+
+    public static Registro()
+    {
+        let xmlh : XMLHttpRequest = new XMLHttpRequest();
+
+        let nombre = (<HTMLInputElement>document.getElementById("nombre")).value;
+    
+        let apellido = (<HTMLInputElement>document.getElementById("apellido")).value;
+
+        let correo = (<HTMLInputElement>document.getElementById("correo")).value;
+    
+        let clave = (<HTMLInputElement>document.getElementById("clave")).value;
+
+        let perfil = (<HTMLInputElement>document.getElementById("perfil")).value;
+
+        var estado = 1;
+
+        let foto : any = (<HTMLInputElement> document.getElementById("foto"));
+
+        let form : FormData = new FormData();
+
+        form.append('foto', foto.files[0]);
+
+        form.append('user','{"nombre":"' + nombre +'", "apellido":"' + apellido +'","perfil":'+ perfil +', "correo":"' + correo +'","estado":'+estado+' ,"clave": "'+clave+'"}');
+
+        xmlh.open("POST","http://localhost/loginPersonal/BACKEND/test_registro.php/registrarUsuario/",true);
+
+        xmlh.setRequestHeader("enctype", "multipart/form-data");
+
+        //xmlh.send('user={"nombre":"' + nombre +'", "apellido":"' + apellido +'","perfil":'+ perfil +', "correo":"' + correo +'","estado":'+estado+' ,"clave": "'+clave+'"}');
+
+        xmlh.send(form);
+        
+        
+        xmlh.onreadystatechange = () => 
+        {
+            if (xmlh.readyState == 4 && xmlh.status == 200) 
+            {
+               console.log(xmlh.responseText);
+            }
+        };
+    
+ 
+    }
+
+}
---
+++ AAProyectoClases/Clase_11_login-jwt-bst/loginPersonal/principal.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   
+
+    <!-- INCLUYO LOS ESTILOS-->
+    <link type="text/css" rel="stylesheet" href="CSS/Estilos.css" />
+
+
+    <title>Pagina principal</title>
+
+</head>
+
+
+<body>
+    <table align="center">
+    <caption><h1>Bienvenido a la pagina principal!</h1></caption>
+
+    <?php
+    session_start();
+    if($_SESSION["perfilUsuario"] == 1)
+    {?>
+        <tr>
+        <td>Listado de usuarios: <a href="test_pdf_usuarios.php"> -> Ingrese aqui</a></td>
+        </tr>
+    <?php
+    }
+    
+    ?>
+    <tr>
+    <td>Listado de productos <a href="test_pdf_productos.php"> -> Ingrese aqui</a></td>
+    </tr>
+    <tr>
+    <td> <a href="cerrarSesion.php">Cerrar sesion</a></td>
+    </tr>
+    </table>   
+</body>
+
+
+</html>
